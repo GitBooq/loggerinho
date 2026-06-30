945aee323683f887d44761d9b65315e6aac3cd7c
Sergey Ants <sergey594m@gmail.com>
2026-06-30 13:54:00 +0300

    separate declarations and definitions of json formatter to put away library dep on nlohmann/json

--- include/formatters/json_formatter.h
+++ include/formatters/json_formatter.h
@@ -1,25 +1,10 @@
 #pragma once
 
 #include "IFormatter.h"
-#include <nlohmann/json.hpp>
 
 namespace formatter {
 class JsonFormatter : public IFormatter {
-public:
-  std::string operator()(const LogRecord &record) const override {
-    using json = nlohmann::json;
-
-    json jSon;
-    jSon["level"] = logLevelToString(record.level);
-    jSon["logger"] = record.loggerName;
-    jSon["msg"] = record.message;
-    jSon["timestamp"] = formatter::formatTimestamp(record.timestamp);
-
-    for (const auto &[key, value] : record.fields) {
-      jSon[key] = value;
-    }
-
-    return jSon.dump(2);
-  }
+ public:
+  std::string operator()(const LogRecord& record) const override;
 };
-} // namespace formatter
+}  // namespace formatter
